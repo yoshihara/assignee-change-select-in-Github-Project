24fce01e52d8a7b395584ddca33b9b6e3c70b019
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-04-13 19:27:08 +0900

    Implement

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # assignee-change-select-in-Github-Project
-GitHub projectでassigneeを切り替えるセレクトボックスを表示するchrome拡張
+
+This chrome extension is buggy snippet, so I will not upload this to Chrome webstrore. If you can use it, please use developer mode in Chrome. (c.f. below "install" section)
+
+※雑な作り方をしているのでストアには上げていません。使いたい方は下のinstallセクションを参考にデベロッパーモードで使ってください
+
+## install
+
+1. git clone or unarchive tarball
+1. Chromeのヘッダー右上にある三点リーダーをクリックする
+1. 「その他ツール」をクリックする
+1. 「拡張機能」を開く
+1. （やっていない場合）「デベロッパーモード」にチェックを入れて、パッケージ化されていない拡張機能を使えるようにする
+1. 「パッケージ化されていない拡張機能を読み込む」をクリックする
+1. このリポジトリを選択して開く
+1. 「G」と書かれたアイコンがブラウザの右上に表示されることを確認する
+
+## how to use
+
+github の projectを開いて2秒後に、assigneeを選択できるセレクトボックスが検索窓の横に表示されます。
+セレクトボックスからassgineeを選択すると、選択したassigneeで絞り込み条件が検索窓に入力され、検索が行われます。
---
+++ content.js
@@ -0,0 +1,68 @@
+"use strict";
+
+// n msec待つ
+const sleep = n => new Promise(resolve => setTimeout(resolve, n));
+
+// ローディングアイコンが消える＝読み込み終了とみなす
+const isLoading = async () => {
+  return !!document
+    .getElementsByClassName("project-columns-container")[0]
+    .querySelectorAll("include-fragment").length;
+};
+
+const waitLoading = async () => {
+  const maxFetchCount = 100;
+  let count = 0;
+
+  // 読み込み終了or100回取得しても消えなかったら以上とみなして終了
+  while ((await isLoading()) && count < maxFetchCount) {
+    await sleep(100);
+    count = count + 1;
+  }
+};
+
+// プロジェクトの読み込みを雑に待つ
+waitLoading().then(() => {
+  // 画面上にあるカードのassigneeを取ってくる
+  // [["hoge"], ["hoge", "fuga"], []...] のような状態になる
+  let assignees = [];
+  document.querySelectorAll("div[data-card-assignee]").forEach(i => {
+    assignees = assignees.concat(JSON.parse(i.dataset.cardAssignee));
+  });
+
+  // 重複削除してソート
+  assignees = Array.from(new Set(assignees)).sort();
+
+  // プロジェクトカードの検索窓の横にセレクトボックスをつける
+  let input = document.querySelector('input[name="card_filter_query"]');
+  let select = document.createElement("select");
+  select.classList = "form-control form-select";
+  document.querySelector(".subnav-search").insertBefore(select, input);
+
+  // 未選択用のoptionをselectに追加
+  select.appendChild(document.createElement("option"));
+  // assigneeをoptionとして入れる
+  assignees.forEach(a => {
+    let op = document.createElement("option");
+    op.innerText = a;
+    op.value = a;
+    select.appendChild(op);
+  });
+
+  // 要素が選ばれたら、その要素の値をassgineeとして検索窓に入れ、検索させる
+  select.onchange = function(selected) {
+    let target = selected.target.options;
+    let value = target[target.selectedIndex].value;
+    let inputValue = "";
+    if (value != "") inputValue = "assignee:" + value;
+    let input = document.querySelector('input[name="card_filter_query"]');
+    input.value = inputValue;
+
+    // 検索窓に対してinputのイベントをdispatchすることで検索を発火させている
+    // 手で検索窓の中身を置き換えた動きを擬似的に再現
+    let inputEvent = new CustomEvent("input");
+    document
+      .querySelector('input[name="card_filter_query"]')
+      .dispatchEvent(inputEvent);
+  };
+});
